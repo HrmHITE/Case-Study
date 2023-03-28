3b8fc7fa2ab948c1ce8f5db3702ec7931e17816a
HrmHITE <hm@hitedigital.com>
2023-03-28 10:59:12 -0600

    Update event-after-submit-download-pdf.js
    CRM BTN changed after hover
    Used MouseOver and MouseOut JQuery Function
    CRM Pages

--- event-after-submit-download-pdf.js
+++ event-after-submit-download-pdf.js
@@ -8,3 +8,17 @@ jQuery("form").submit(function(event){
     a.download = 'Arrow Boutique.pdf';
     a.click();
 });
+
+
+//CRM BTN changed after hover
+//Used MouseOver and MouseOut JQuery Function
+
+jQuery(document).ready(function(){
+    jQuery("#btncrmmain").mouseover(function(){
+        jQuery(this).html("Try FREE for 14 days<br><span>or book a demo with one of our specialists.</span>");
+        jQuery(this).find("span").css({"font-size":"12px","color":"#182150"});
+    });
+    jQuery("#btncrmmain").mouseout(function(){
+        jQuery(this).text("Try FREE for 14 days");
+    });
+});
